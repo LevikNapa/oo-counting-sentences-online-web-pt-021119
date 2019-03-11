bfbe1814b30478a17db620af1b719c283360c4cf
Levik Naparstek <levik010@gmail.com>
2019-03-11 20:35:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+    
+  #   if self.end_with?("!", ".", "?") == true
+  #     self.count(3).split(" ")
+  #   end
+  # end
+  array = self.split(/[!?.]/).reject do |string|
+    string.empty?
+    
   end
+  array.length
+end
 end
